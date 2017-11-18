b4b521d872d48acbcecb4f27752f50c077ef3d35
weibin268 <binweizhuang@hotmail.com>
2017-11-18 20:58:30 +0800

    aa

--- zhuang/algorithm/sort.py
+++ zhuang/algorithm/sort.py
@@ -1,5 +1,6 @@
-def qsort(array, begin, end):
+import time
 
+def qsort(array, begin, end):
     if (end - begin) == 1:
         if array[end] > array[begin]:
             temp = array[begin]
@@ -30,15 +31,29 @@ def qsort(array, begin, end):
 
             a = a + 1
             b = b - 1
+            time.sleep(1)
+            print(array)
 
-    if begin<(pIndex-1):
+
+
+    if begin < (pIndex - 1):
         qsort(array, begin, pIndex - 1)
-    if (pIndex+1)<end:
+    if (pIndex + 1) < end:
         qsort(array, pIndex + 1, end)
 
 
-nums = [11, 2, 1, 34, 12, 34, 134, 566, 11, 3234, 123, 44, 51]
+
+import random
+
+nums = []
+for i in range(10):
+    nums.append(random.randint(0, 100))
+
+print(nums)
 
 qsort(nums, 0, len(nums) - 1)
 
-print(nums)
+#print(nums)
+
+
+
