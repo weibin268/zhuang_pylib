d1d44c65f8cf6aa5236838f88929b9517196d24f
weibin268 <binweizhuang@hotmail.com>
2018-05-12 12:53:56 +0800

    aa

--- tests/data/test_redis.py
+++ tests/data/test_redis.py
@@ -1,6 +1,6 @@
 import redis
 
-con= redis.Redis(host='127.0.0.1',port=6379);
+con= redis.Redis(host='127.0.0.1',port=6379)
 
 print(con)
 
--- tests/web/test_http.py
+++ tests/web/test_http.py
@@ -5,7 +5,7 @@ from zhuang.web.http import HttpClient
 class TestHttpClient(unittest.TestCase):
 
     def test_post(self):
-        client = HttpClient("https://www.baidu.com");
+        client = HttpClient("https://www.baidu.com")
         result = client.post("")
         print(result)
         self.assertEqual(1,1,'error')
